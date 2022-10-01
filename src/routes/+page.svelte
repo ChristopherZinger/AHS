cc3fd8869653bd8f13692b1f83db36dc33ddb947
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-10-01 13:48:51 +0200

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import LoginForm from './components/auth/LoginForm.svelte';
+	import TestFetch from './components/TestFetch.svelte';
+</script>
+
+<h3>Home</h3>
+
+<LoginForm />
+<TestFetch />
