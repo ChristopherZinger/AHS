fb34f811698454bb0eec920e08a8af46f6b2d559
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-10-08 21:02:05 +0200

    Button styles

---
+++ src/components/Button.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let type = '';
+</script>
+
+<button
+	class={`
+    flex justify-center place-items-center align-middle   
+    border-2 border-black rounded-full
+    text-xl font-medium
+    duration-100
+    hover:text-white hover:bg-black`}
+	{type}
+>
+	<slot />
+</button>
+
+<style>
+	button {
+		width: 223px;
+		height: 51px;
+	}
+</style>
