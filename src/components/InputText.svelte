8cbae3c0ba0b902eab690e80df79da93add80bb9
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-10-08 21:03:12 +0200

    InputText component

---
+++ src/components/InputText.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let type: 'text' | 'password' = 'text';
+	export let id: string;
+	export let name: string;
+	export let value = '';
+	export let onChange: (v: { target: any | null }) => void;
+</script>
+
+<input
+	class={`
+  w-full
+  px-2.5 py-1.5
+  border-b-2 border-black
+`}
+	{type}
+	{id}
+	{name}
+	{value}
+	on:input={onChange}
+/>
+
+<style>
+	input {
+		outline: none;
+	}
+</style>
