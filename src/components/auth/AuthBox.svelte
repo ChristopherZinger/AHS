b7f220faf6eb48b44886c26cf383ba86db2bc8be
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-10-15 09:59:51 +0200

    add slot to AuthBox.svelte

--- src/components/auth/AuthBox.svelte
+++ src/components/auth/AuthBox.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-	import LoginForm from './LoginForm.svelte';
 	export let title: string;
 </script>
 
 <div
 	class={`
-  auth-box py-20
-  lg:px-12
-  lg:border lg:rounded-2xl lg:border-2 lg:border-black
+    auth-box py-20
+    lg:px-12
+    lg:border lg:rounded-2xl lg:border-2 lg:border-black
   `}
 >
 	<h1 class="font-bold text-2xl mb-12">{title}</h1>
-	<LoginForm />
+	<slot />
 </div>
 
 <style>
