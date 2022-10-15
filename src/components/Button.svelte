a437ffe9a974f7a152f8d987ed900377d5e05df4
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-10-15 12:30:07 +0200

    add disabled state to Button.svelte

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	export let type = '';
+	export let disabled = false;
 </script>
 
 <button
+	{disabled}
 	class={`
     flex justify-center place-items-center align-middle   
-    border-2 border-black rounded-full
+    border-2 ${disabled ? 'border-zinc-500' : 'border-black'} rounded-full
+    ${disabled && 'cursor-not-allowed'}
+    ${disabled && 'text-zinc-500'}
     text-xl font-medium
     duration-100
-    hover:text-white hover:bg-black`}
+    ${!disabled && 'hover:text-white hover:bg-black'}`}
 	{type}
 >
 	<slot />
