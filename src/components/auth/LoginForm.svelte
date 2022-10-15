283d136acbfa80a97026d85f15359496c3fdc937
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-10-15 12:31:25 +0200

    add input validation to login form

--- src/components/auth/LoginForm.svelte
+++ src/components/auth/LoginForm.svelte
@@ -4,42 +4,84 @@
 	import Button from '../Button.svelte';
 	import InputText from '../InputText.svelte';
 	import JumpingLabel from '../JumpingLabel.svelte';
+	import * as yup from 'yup';
+	import { MIN_PASSWORD_LENGTH } from '$lib/constants';
+	import { parseValidationError, validate } from '$lib/utils/form-utils';
+	import InputErrors from './InputErrors.svelte';
 
-	let email = '';
-	let password: '';
+	const values = {
+		email: '',
+		password: ''
+	};
+	let inputErrors: Partial<Record<keyof typeof values, string[]>> = {};
+	let isLoading = false;
+
+	const schema = yup.object({
+		email: yup.string().email().required(),
+		password: yup.string().min(MIN_PASSWORD_LENGTH).required()
+	});
 
 	async function submit() {
-		await signInWithEmailAndPassword(auth, email, password);
+		inputErrors = {};
+		isLoading = true;
+		try {
+			await validate(values, schema);
+			await signInWithEmailAndPassword(
+				auth,
+				values.email,
+				values.password
+			);
+		} catch (err) {
+			err instanceof yup.ValidationError
+				? (inputErrors = parseValidationError(err))
+				: console.error(err);
+		}
+		isLoading = false;
 	}
 </script>
 
 <form class="flex flex-col gap-y-12" on:submit|preventDefault={submit}>
-	<JumpingLabel label="*Email" forHTML="email" isUp={!!email}>
+	<JumpingLabel label="*Email" forHTML="email" isUp={!!values.email}>
 		<InputText
 			type={'text'}
 			name="email"
 			id="email"
 			onChange={({ target }) => {
-				email = target?.value;
+				values.email = target?.value;
+				inputErrors.email = [];
 			}}
 		/>
+		<InputErrors msgs={inputErrors.email || []} />
 	</JumpingLabel>
 
-	<JumpingLabel label="*Password" forHTML="password" isUp={!!password}>
+	<JumpingLabel
+		label="*Password"
+		forHTML="password"
+		isUp={!!values.password}
+	>
 		<InputText
 			type={'password'}
 			name="password"
 			id="password"
 			onChange={({ target }) => {
-				password = target?.value;
+				values.password = target?.value;
+				inputErrors.password = [];
 			}}
 		/>
+		<InputErrors msgs={inputErrors.email || []} />
 	</JumpingLabel>
 
 	<div class="flex justify-center mt-8">
 		<div>
-			<Button type="submit">login</Button>
-			<div class="flex mt-5 text-zinc-500">Forgot password? Reset.</div>
+			<Button
+				type="submit"
+				disabled={isLoading ||
+					!!Object.values(inputErrors).some((arr) => arr.length)}
+				>login</Button
+			>
+			<div class="flex mt-5 text-zinc-500">
+				Forgot password? &nbsp <span class="text-black">Reset.</span>
+			</div>
 		</div>
 	</div>
 </form>
