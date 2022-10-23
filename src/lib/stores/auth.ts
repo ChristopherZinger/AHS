14671fc63a3073b9b6f498c79a102c98fafac572
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-10-23 12:30:28 +0200

    handle id token change - get session cookie from endpoint on id token change

--- src/lib/stores/auth.ts
+++ src/lib/stores/auth.ts
@@ -1,26 +1,44 @@
-import { readable } from 'svelte/store'
-import { auth } from '$lib/firebase'
-import { onAuthStateChanged, onIdTokenChanged } from 'firebase/auth'
-import { browser } from '$app/environment'
+import { readable } from 'svelte/store';
+import { auth } from '$lib/firebase-client';
+import { onAuthStateChanged, onIdTokenChanged } from 'firebase/auth';
+import { browser } from '$app/environment';
+import { logout } from '$lib/auth-client-utils';
 
 type appUser = {
-  email: string
-}
+	email: string;
+};
 
-export const appUser = readable<appUser | null | undefined>(undefined, (set) => {
-  console.log('appUser store')
-  if (browser) {
-    onAuthStateChanged(auth, (user) =>
-      set(user ? { email: user.email || '' } : null)
-    )
-  }
-})
+export const appUser = readable<appUser | null | undefined>(
+	undefined,
+	(set) => {
+		if (browser) {
+			onAuthStateChanged(auth, (user) =>
+				set(user ? { email: user.email || '' } : null)
+			);
+		}
+	}
+);
 
-export const fs_token = readable<string | null | undefined>(undefined, (set) => {
-  console.log('fs_token store')
-  if (browser) {
-    onIdTokenChanged(auth, async (token) =>
-      set(token ? await token.getIdToken() : null)
-    )
-  }
-})
+export const fs_token = readable<string | null | undefined>(
+	undefined,
+	(set) => {
+		if (browser) {
+			onIdTokenChanged(auth, async (token) => {
+				const tokenId = token ? await token.getIdToken() : null;
+				set(tokenId);
+
+				if (tokenId) {
+					const res = await fetch('/api/handleTokenUpdate', {
+						body: tokenId,
+						method: 'POST'
+					});
+
+					if (res.status === 401) {
+						console.error('Could not verify the user');
+						await logout();
+					}
+				}
+			});
+		}
+	}
+);
