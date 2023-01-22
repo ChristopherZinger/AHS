7c23f2c1c9bd1955dc1cd4e5d05e4b5ba3a2b23f
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-01-22 10:45:29 +0100

    create Dropdown component

---
+++ src/lib/components/shared/Dropdown.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { clickOutside } from '$lib/actions/outclick';
+
+	let isFocused = false;
+</script>
+
+<div
+	on:focusin={() => {
+		isFocused = true;
+	}}
+	use:clickOutside={(hi) => {
+		isFocused = false;
+	}}
+>
+	<slot name="input" />
+
+	<div class="relative w-full" class:hidden={!isFocused}>
+		<div class="drop-down absolute">
+			<slot name="options" />
+		</div>
+	</div>
+</div>
+
+<style>
+	.drop-down {
+		width: 100%;
+		max-height: 400px;
+		overflow-x: hidden;
+		overflow-y: auto;
+	}
+</style>
