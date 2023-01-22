f304fff174b4886dbac0eef18474a1f0a7b94c3f
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-01-22 15:48:46 +0100

    add CancellIcon component

---
+++ src/lib/components/icons/CancelIcon.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let color: string = 'black';
+	export let height: string = '12px';
+</script>
+
+<svg
+	width="11"
+	{height}
+	viewBox="0 0 11 12"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<path d="M1 1.5L10 10.5" stroke={color} stroke-width="2" />
+	<path d="M1 10.5L10 1.5" stroke={color} stroke-width="2" />
+</svg>
