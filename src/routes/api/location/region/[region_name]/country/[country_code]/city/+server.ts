0fe20f40783acfb62621b8d620ea9e41c97d648c
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-01-22 15:47:27 +0100

    create cities api endpoint

---
+++ src/routes/api/location/region/[region_name]/country/[country_code]/city/+server.ts
@@ -0,0 +1,30 @@
+import { prisma } from '$lib/prisma';
+import { json, error } from '@sveltejs/kit';
+import { z } from 'zod';
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ params }) {
+	const countryCodeQueryParam = params['country_code'];
+	const parseResult = z
+		.string()
+		.length(2)
+		.safeParse(countryCodeQueryParam);
+
+	if (!parseResult.success) {
+		throw error(400, 'Bad request');
+	}
+
+	try {
+		const cities = await prisma.city.findMany({
+			where: {
+				countryAlpha2: parseResult.data
+			}
+		});
+
+		return json({
+			cities
+		});
+	} catch (err) {
+		throw error(500, err as any);
+	}
+}
