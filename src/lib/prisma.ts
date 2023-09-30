35aaec5130f32a6b3afdc134671cad635c34cd76
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-09-30 12:34:09 +0200

    improve userPrisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,12 +2,18 @@ import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
 
-export async function usePrisma(cb: (p: typeof prisma) => Promise<void>) {
+export const getPrismaClient = () => new PrismaClient();
+
+export type AppPrisma = typeof prisma;
+
+export async function usePrisma(
+	cb: (p: AppPrisma) => Promise<void>
+): Promise<void> {
 	const prisma = new PrismaClient();
 	try {
 		await cb(prisma);
 	} catch (e) {
 		prisma.$disconnect();
-		throw new Error(e);
+		throw new Error(e as any);
 	}
 }
