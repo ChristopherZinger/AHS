07f1aca1c1f6e99b411d43ba055adc50f0a0eb12
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-09-30 09:55:25 +0200

    hange /thanks page location

--- src/routes/survey-thanks/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-</script>
-
-<h1>Thanks</h1>
-<p>For completing the survey</p>
---
+++ src/routes/survey/thanks/+page.svelte
@@ -0,0 +1,14 @@
+<div class="app-section__narrow ">
+	<h1 class="text-4xl font-bold my-10">Dzięki!</h1>
+
+	<p class="my-3">
+		Dzięki za wypełnienie ankiety. Do zobaczenia niebawem!
+	</p>
+
+	<p>
+		Masz ciekawe doświadczenia z innch biur?
+		<a href="/survey" class="font-semibold underline">
+			wypełnij ponownie
+		</a>
+	</p>
+</div>
