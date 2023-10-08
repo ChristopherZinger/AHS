6e353fa644dc288798dcfcdecd91c8520fda721d
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-10-08 11:24:51 +0200

    survey - office name input refinement

--- src/routes/survey/SurveyOfficeInfo.svelte
+++ src/routes/survey/SurveyOfficeInfo.svelte
@@ -10,6 +10,7 @@
 	export let initialCity: string;
 	export let onClearErrors: (field: string | number) => void;
 
+	let officeName = initialOfficeName;
 	function _onCityChange(value: string) {
 		onCityChange(value);
 		if (inputErrors.city) {
@@ -18,7 +19,9 @@
 	}
 
 	function _onOfficeNameChange(value: string) {
-		onOfficeNameChange(value);
+		const _officeName = value.replace('<', ' ').replace('>', ' ');
+		officeName = _officeName;
+		onOfficeNameChange(_officeName);
 		if (inputErrors.officeName) {
 			onClearErrors('officeName');
 		}
@@ -32,17 +35,26 @@
 		id="officeName"
 		name="officeName"
 		value={initialOfficeName}
+		placeholder={`np. "APA Janusz i Architekci"`}
 	/>
 	<InputErrors msgs={inputErrors.officeName || []} />
 </JumpingLabel>
 
-<JumpingLabel label="W jakim miescie:" forHTML="city">
+<JumpingLabel
+	label={`W jakim miescie znajduje się ${
+		officeName
+			? `<b><pre class="inline">${officeName}</pre></b>`
+			: ' to biuro'
+	}?`}
+	forHTML="city"
+>
 	<InputText
 		type="text"
 		onChange={_onCityChange}
 		id="city"
 		name="city"
 		value={initialCity}
+		placeholder={`np. "Warszawa"`}
 	/>
 	<InputErrors msgs={inputErrors.city || []} />
 </JumpingLabel>
