2d5fe64f2dfd658a80da5a051823d728978d9e21
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-10-22 16:10:13 +0200

    create new survey form wrapper

---
+++ src/routes/survey/SurveyForm.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { SurveyForm } from '$lib/utils/surveyTypes';
+	import SurveySubmitAndNavigationButton from './SurveySubmitAndNavigationButton.svelte';
+
+	export let actionName: string;
+	export let currentStep: number;
+	export let onSubmitSuccess: (v: SurveyForm) => void;
+	export let onSubmitError: (e: {
+		type: 'error';
+		status?: number | undefined;
+		error: any;
+	}) => void;
+
+	let isLoading = false;
+</script>
+
+<form
+	action={`?/${actionName}`}
+	method="POST"
+	use:enhance={() => {
+		isLoading = true;
+		return ({ result }) => {
+			if (result.type === 'success') {
+				onSubmitSuccess(result.data);
+			} else if (result.type === 'error') {
+				onSubmitError(result);
+			}
+			isLoading = false;
+		};
+	}}
+>
+	<slot />
+
+	<SurveySubmitAndNavigationButton
+		{currentStep}
+		isDisabled={isLoading}
+		{isLoading}
+		{onSubmitSuccess}
+		{onSubmitError}
+	/>
+</form>
