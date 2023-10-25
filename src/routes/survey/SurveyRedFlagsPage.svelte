1a4a76152423baa138c5d70c6a6c1a0a30382e42
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-10-25 20:33:16 +0200

    split red flags survey into steps

--- src/routes/survey/SurveyRedFlagsPage.svelte
+++ src/routes/survey/SurveyRedFlagsPage.svelte
@@ -1,21 +1,30 @@
 <script lang="ts">
 	import SurveyForm, { type ActionErrorResult } from './SurveyForm.svelte';
-	import SurveyRedFlags from './SurveyRedFlags.svelte';
 	import type { SurveyForm as SurveyFormType } from '$lib/utils/surveyTypes';
+	import { redFlagSectionsInOrder } from '$lib/utils/redFlagUtils';
+	import { getRedFlagSectionNameLabel } from '$lib/components/labels/RedFlagSectionLabel.svelte';
+	import Checkbox from '$lib/components/inputs/Checkbox.svelte';
+	import { getRedFlagNameLabel } from '$lib/components/labels/RedFlagLabel.svelte';
 
 	export let officeName: string;
 	export let currentStep: number;
 	export let initialFlags: Record<string, boolean>;
 	export let onSubmitSuccess: (v: SurveyFormType) => void;
 	export let onSubmitError: (e: ActionErrorResult) => void;
+
+	$: redFlagStep = currentStep - 2;
 </script>
 
-<h1 class="text-4xl font-bold my-10">Czerwone flagi</h1>
+<h1 class="text-4xl font-bold my-10">
+	Czerwone flagi - {getRedFlagSectionNameLabel(
+		redFlagSectionsInOrder[redFlagStep].section
+	)}
+</h1>
 <p class="lg:text-2xl my-5">
 	Zaznacz czerwone flagi (red flags) napotkane podczas pracy w <b
 		>{officeName}</b
-	>. Czerwone flagi to problemy z jakimi spotykasz się w biurze
-	architektonicznym, ale o których niekoniecznie wiesz przed podjęciem
+	>. Czerwone flagi to problemy z jakimi spotykasz się w pracowni
+	architektonicznej, ale o których niekoniecznie wiesz przed podjęciem
 	pracy.
 </p>
 
@@ -25,5 +34,33 @@
 	{onSubmitSuccess}
 	{currentStep}
 >
-	<SurveyRedFlags flagsToBoolean={initialFlags} />
+	<div>
+		{#each redFlagSectionsInOrder as section}
+			<div
+				class:hidden={redFlagSectionsInOrder[redFlagStep].section !==
+					section.section}
+				class="flex flex-col gap-2"
+			>
+				{#each section.flags as flag}
+					<div
+						class:bg-red-400={initialFlags[flag]}
+						class="rounded p-3 duration-75"
+						class:text-white={initialFlags[flag]}
+					>
+						<Checkbox
+							name={flag}
+							label={getRedFlagNameLabel(flag)}
+							bind:value={initialFlags[flag]}
+						/>
+					</div>
+				{/each}
+			</div>
+		{/each}
+	</div>
 </SurveyForm>
+
+<style>
+	.hidden {
+		display: none;
+	}
+</style>
