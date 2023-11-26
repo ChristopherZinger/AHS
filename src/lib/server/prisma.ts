e0577976706f362970f300c059d62c24a45e4514
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-11-26 15:02:06 +0100

    remove unused usePrisma function

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -2,13 +2,4 @@ import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
 
-export async function usePrisma(
-	cb: (p: typeof prisma) => Promise<void>
-): Promise<void> {
-	try {
-		await cb(prisma);
-	} catch (e) {
-		prisma.$disconnect();
-		throw new Error(e as any);
-	}
-}
+export const getPrisma = () => new PrismaClient();
