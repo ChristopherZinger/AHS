2619a2b1ccb2fc00d4a512ee8721f18911eaf57f
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-11-26 14:50:54 +0100

    init firebase in the frontend

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,31 @@
 	import './styles.css';
 	import TopNav from '$lib/components/nav/TopNav.svelte';
 	import MobileSideNav from '$lib/components/nav/MobileSideNav.svelte';
+	import { appUser, type AppUser } from '$lib/stores/auth';
+	import {
+		initializeAuth,
+		getAuth,
+		onAuthStateChanged
+	} from 'firebase/auth';
+	import { onMount } from 'svelte';
+	import { initializeApp } from 'firebase/app';
+	import * as fbConfig from '../../firebase.config.json';
+
+	onMount(() => {
+		const app = initializeApp(fbConfig);
+		initializeAuth(app);
+
+		onAuthStateChanged(getAuth(), (u) => {
+			appUser.set(
+				u
+					? {
+							email: u.email,
+							id: u.uid
+					  }
+					: null
+			);
+		});
+	});
 
 	$: isMobileMenuOpen = false;
 	const setIsMobileMenuOpen = (v: boolean) => {
